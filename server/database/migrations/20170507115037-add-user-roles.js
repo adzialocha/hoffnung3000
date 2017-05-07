db2c6c001bbaa54db95a35afa47f36e7a6c6ab84
Andreas Dzialocha <andreas@marmorkuchen.net>
2017-05-07 14:03:03 +0200

    Add isAdmin and isParticipant role flags to user model

---
+++ server/database/migrations/20170507115037-add-user-roles.js
@@ -0,0 +1,27 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.addColumn(
+      'users',
+      'isAdmin',
+      {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      }
+    )
+
+    queryInterface.addColumn(
+      'users',
+      'isParticipant',
+      {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      }
+    )
+  },
+  down: (queryInterface) => {
+    queryInterface.removeColumn('users', 'isAdmin')
+    queryInterface.removeColumn('users', 'isParticipant')
+  },
+}
