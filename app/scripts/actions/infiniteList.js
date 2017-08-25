d3a930c79fad911299b4c2a2bfea7919f9f9b221
Andreas Dzialocha <andreas@marmorkuchen.net>
2017-08-25 11:40:55 +0200

    Increase limit for conversations

--- app/scripts/actions/infiniteList.js
+++ app/scripts/actions/infiniteList.js
@@ -3,10 +3,11 @@ import { getRequest } from '../services/api'
 import { UPDATE_USER_STATUS } from '../middlewares/userStatus'
 
 export const ITEMS_PER_PAGE = 50
+export const CONVERSATIONS_PER_PAGE = 500
 
 export function fetchList(path, page = 0, params = {}) {
-  const offset = page * ITEMS_PER_PAGE
-  const limit = ITEMS_PER_PAGE
+  let offset = page * ITEMS_PER_PAGE
+  let limit = ITEMS_PER_PAGE
   const meta = {
     page,
     offset,
@@ -27,6 +28,10 @@ export function fetchList(path, page = 0, params = {}) {
 
   if (requestPath[0] === 'conversations') {
     success[UPDATE_USER_STATUS] = true
+
+    // this is a quick hack fixing a wierd SQL issue
+    offset = page * CONVERSATIONS_PER_PAGE
+    limit = CONVERSATIONS_PER_PAGE
   }
 
   return getRequest(requestPath, { offset, limit, ...params }, {
