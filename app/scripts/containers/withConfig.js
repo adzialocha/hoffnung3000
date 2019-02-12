d4c68c9007d8e6e9299390460f48418898bea5d0
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-02-12 19:01:44 +0100

    Pass on props when using withConfig container, add comments and required config warning

--- app/scripts/containers/withConfig.js
+++ app/scripts/containers/withConfig.js
@@ -4,14 +4,31 @@ import { connect } from 'react-redux'
 
 import { updateMetaInformation } from '../actions/meta'
 
+/**
+ * Assures the config is fetched from server and ready to be used by a wrapped
+ * component. Hides the component or shows a warning when configuration was
+ * required but not found or set.
+ */
 export default function withConfig(...args) {
   let field
+  let isRequired = false
   let WrappedComponent
 
   if (args.length === 2 && typeof args[0] === 'string') {
+    // Ask for a certain config, hide component when not set
     field = args[0]
     WrappedComponent = args[1]
+  } else if (
+    args.length === 3 &&
+    typeof args[0] === 'string' &&
+    typeof args[1] === 'boolean'
+  ) {
+    // Ask for a certain config and show a warning when not set
+    field = args[0]
+    isRequired = args[1]
+    WrappedComponent = args[2]
   } else {
+    // Just load the config object
     WrappedComponent = args[0]
   }
 
@@ -35,12 +52,16 @@ export default function withConfig(...args) {
         return null
       }
 
-      // Do not render component when config field was not set
       if (field && !(field in this.props.config)) {
+        if (isRequired) {
+          return <p>Warning! <em>{ field }</em> is not set. Please check your configuration.</p>
+        }
+
+        // Do not render component when config field was not set
         return null
       }
 
-      return <WrappedComponent config={this.props.config} />
+      return <WrappedComponent config={this.props.config} {...this.props} />
     }
   }
 
