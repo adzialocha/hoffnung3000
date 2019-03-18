395d4fe3632fbbf17de053b7f8244eb47286b8dd
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-03-18 01:07:26 +0100

    Set timezone explicitly to UTC on server-side

--- server/database/config.js
+++ server/database/config.js
@@ -6,17 +6,22 @@ dotenv.config({ path: path.join(__dirname, '..', '..', '.env') })
 const url = process.env.DATABASE_URL
 const dialect = process.env.DATABASE_DIALECT || 'postgres'
 
+const timezone = '+00:00' // UTC
+
 module.exports = {
   development: {
     url,
     dialect,
+    timezone,
   },
   staging: {
     url,
     dialect,
+    timezone,
   },
   production: {
     url,
     dialect,
+    timezone,
   },
 }
