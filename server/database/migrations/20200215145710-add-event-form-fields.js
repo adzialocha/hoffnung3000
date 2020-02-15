a23534208c941c70918c6d9c2e5e3b789e3f96ec
Sam Andreae <sandreae@gmail.com>
2020-02-15 16:41:03 +0000

    migration change

---
+++ server/database/migrations/20200215145710-add-event-form-fields.js
@@ -0,0 +1,26 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.addColumn(
+        'events',
+        'socialMedia',
+        {
+          type: Sequelize.STRING,
+        }
+      ),
+      queryInterface.addColumn(
+        'events',
+        'websiteUrl',
+        {
+          type: Sequelize.STRING,
+        }
+      ),
+    ])
+  },
+  down: queryInterface => {
+    return Promise.all([
+      queryInterface.removeColumn('events', 'socialMedia'),
+      queryInterface.removeColumn('events', 'websiteUrl'),
+    ])
+  },
+}
