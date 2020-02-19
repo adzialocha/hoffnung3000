56e470a3167bed5b05cbb610be32ea9708068cea
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-02-19 21:58:50 +0100

    Remove alphanum restriction in user firstname and lastname validation

--- server/validation/user.js
+++ server/validation/user.js
@@ -4,16 +4,16 @@ export default {
   createUser: {
     body: {
       email: Joi.string().email().required(),
-      firstname: Joi.string().alphanum().min(3).max(30).required(),
-      lastname: Joi.string().alphanum().min(3).max(30).required(),
+      firstname: Joi.string().min(3).max(30).required(),
+      lastname: Joi.string().min(3).max(30).required(),
       password: Joi.string().min(8).required(),
     },
   },
   updateUser: {
     body: {
       email: Joi.string().email().required(),
-      firstname: Joi.string().alphanum().min(3).max(30).required(),
-      lastname: Joi.string().alphanum().min(3).max(30).required(),
+      firstname: Joi.string().min(3).max(30).required(),
+      lastname: Joi.string().min(3).max(30).required(),
     },
     params: {
       resourceId: Joi.string().hex().required(),
