f659e74987bcd9bfca46f872f796ee3c97bd6f93
Sam Andreae <sandreae@gmail.com>
2020-03-06 19:06:52 +0000

    Correction: PUT and DELETE in wrong route

--- server/routes/event.js
+++ server/routes/event.js
@@ -4,7 +4,7 @@ import validate from 'express-validation'
 import eventController from '../controllers/event'
 import eventValidation from '../validation/event'
 
-import { canReadAsVisitor, canCreate, canUpdate, canDelete } from '../middlewares/roles'
+import { canCreate, canUpdate, canDelete } from '../middlewares/roles'
 import { grantWhenFestivalFree } from '../middlewares/authorizeFreeFestival'
 import { authorizeJWT } from '../middlewares/authorizeJWT'
 import { checkUser } from '../middlewares/checkUser'
@@ -16,12 +16,20 @@ router.route('/')
     checkUser,
     grantWhenFestivalFree,
     eventController.findAll
-  )  
+  )
   .post(
     authorizeJWT,
     canCreate,
     validate(eventValidation.createEvent),
     eventController.create
+  )
+
+router.route('/:resourceSlug')
+  .get(
+    checkUser,
+    grantWhenFestivalFree,
+    eventController.lookup,
+    eventController.findOneWithSlug
   )
   .put(
     authorizeJWT,
@@ -35,14 +43,6 @@ router.route('/')
     eventController.lookup,
     canDelete,
     eventController.destroy
-  )
-
-router.route('/:resourceSlug')
-  .get(
-    checkUser,
-    eventController.lookup,
-    grantWhenFestivalFree,
-    eventController.findOneWithSlug
   )
 
 export default router
