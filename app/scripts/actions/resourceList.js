67d025990d914c861b9464478342dc23386c2e61
Sam Andreae <sandreae@gmail.com>
2020-03-09 19:19:59 +0000

    create actions and reducer for fetchAllResources API call

---
+++ app/scripts/actions/resourceList.js
@@ -0,0 +1,25 @@
+import ActionTypes from '../actionTypes'
+import { getRequest } from '../services/api'
+
+export function fetchResourceList(resourceType) {
+  const data = { fetchAll: true }
+  const meta = {
+    resourceType,
+    data,
+  }
+
+  return getRequest([resourceType], data, {
+    request: {
+      type: ActionTypes.RESOURCE_LIST_REQUEST,
+      meta,
+    },
+    success: {
+      type: ActionTypes.RESOURCE_LIST_SUCCESS,
+      meta,
+    },
+    failure: {
+      type: ActionTypes.RESOURCE_LIST_FAILURE,
+      meta,
+    },
+  })
+}
