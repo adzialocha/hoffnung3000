809fd75cc0ba2b5dcc82e07aa0d87194859de401
Sam Andreae <sandreae@gmail.com>
2020-03-11 22:47:54 +0000

    Include FormTagSelector in EventForm

--- app/scripts/forms/EventForm.js
+++ app/scripts/forms/EventForm.js
@@ -10,6 +10,7 @@ import {
   FormInput,
   FormPlaceSlotSelector,
   FormResourceSelector,
+  FormTagSelector,
   FormTextarea,
 } from '../components'
 
@@ -159,6 +160,22 @@ class EventForm extends Component {
     )
   }
 
+  renderFormTagSelector() {
+    const defaultTags = this.props.config.defaultTags.map(tag =>{
+      return { label: tag, value: tag }
+    })
+
+    return (
+      <Field
+        component={FormTagSelector}
+        defaultTags={defaultTags}
+        disabled={this.props.isLoading}
+        multi={true}
+        name="tags"
+      />
+    )
+  }
+
   render() {
     return (
       <form className="form" onSubmit={this.props.handleSubmit}>
@@ -205,6 +222,10 @@ class EventForm extends Component {
           name="additionalInfo"
           type="text"
         />
+
+        <h2>{translate('forms.event.tags')}</h2>
+
+        { this.renderFormTagSelector() }
 
         <hr />
         <h2>{ translate('forms.common.uploadImages') }</h2>
