515963c5697ca38df79fb426396e3c45e1dbcff8
Sam Andreae <sandreae@gmail.com>
2020-03-11 22:44:13 +0000

    Create TagSelector component and child for Event Forms

---
+++ app/scripts/components/FormTagSelectorItem.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class FormTagSelectorItem extends Component {
+  static propTypes = {
+    label: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    tagArray: PropTypes.string.isRequired,
+    thisTag: PropTypes.string.isRequired,
+  }
+
+  onSelect(selectedTags) {
+    this.props.onChange(selectedTags)
+  }
+
+  render() {
+    const { tagArray, label, thisTag } = this.props
+    return (
+      <div
+        className={JSON.parse(tagArray).includes(thisTag) === true ? 'tag tag--selected' : 'tag'}
+        onClick={this.handleClick}
+      >
+        {label}
+      </div>
+    )
+  }
+
+  updateTags(selectedTags, clickedTag, setSelectedTags) {
+    if (clickedTag && selectedTags) {
+      if (selectedTags.includes(clickedTag)) {
+        return setSelectedTags(selectedTags.filter(item => item !== clickedTag))
+      }
+      selectedTags.push(clickedTag)
+      return setSelectedTags(selectedTags)
+    }
+    return false
+  }
+
+  handleClick() {
+    this.updateTags(JSON.parse(this.props.tagArray), this.props.thisTag, this.onSelect)
+  }
+
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+    this.onSelect = this.onSelect.bind(this)
+  }
+}
+
+export default FormTagSelectorItem
