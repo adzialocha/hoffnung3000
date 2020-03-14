6ec42044f3754b392645fa3a973e0df15e72749f
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-03-14 23:24:46 +0100

    Make linter happy

--- app/scripts/components/Header.js
+++ app/scripts/components/Header.js
@@ -13,4 +13,4 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default Header
--- server/middlewares/getUserWhenAuthenticated.js
+++ server/middlewares/getUserWhenAuthenticated.js
@@ -1,11 +1,11 @@
 import passport from '../services/passport'
 
 // This for setting the user value when the festival is free for checing if user is owner later.
-
 export function getUserWhenAuthenticated(req, res, next) {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err) {
-      return next(err)
+      next(err)
+      return
     }
 
     if (user) {
