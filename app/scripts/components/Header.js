174c5c45a584f928803d473dba12fbfb2f730fd8
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2020-03-14 23:20:13 +0100

    Add logo to header and change font family

--- app/scripts/components/Header.js
+++ app/scripts/components/Header.js
@@ -1,23 +1,16 @@
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-import { withConfig } from '../containers'
-
 class Header extends Component {
-  static propTypes = {
-    config: PropTypes.object.isRequired,
-  }
-
   render() {
     return (
       <header role="banner">
         <div className="logo">
-          <Link to="/">{ this.props.config.title }</Link>
+          <Link to="/" />
         </div>
       </header>
     )
   }
 }
 
-export default withConfig(Header)
+export default Header;
